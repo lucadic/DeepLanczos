797054fbcf9bb3b6245344e2e4e62054cd2b5543
Luca Di Carlo Della <ld1440@princeton.edu>
2025-07-15 11:36:44 -0400

    Adding kwargs

--- deeplanczos/src/deep_lanczos/power_method.py
+++ deeplanczos/src/deep_lanczos/power_method.py
@@ -22,6 +22,8 @@ def orthogonlize(P, Q):
     Q_norm = dot_product(Q, Q)
     return jtu.tree_map(lambda p, q : p - q * cdot/(Q_norm), P, Q) 
 
+def norm2(P): 
+    return dot_product(P,P)
 
 def normalize(P): 
     P_norm = dot_product(P, P)**0.5
@@ -48,20 +50,24 @@ def ComputeHVP (loss, par, batch, V, batch_parser=standard_parser):
     return HVP 
 
 
-def PowerMethodStep(loss, par, batch_iterator, V, batch_parser=standard_parser, show_pbar=False):
+def PowerMethodStep(loss, par, batch_iterator, V, **kwargs):
     HVP = initializeHVP(par)
+    show_pbar = kwargs.get('show_pbar', False)
+    batch_parser = kwargs.get('batch_parser', standard_parser)
+    momentum = kwargs.get('momentum', 0)
     count = 0
     iterator = batch_iterator if not show_pbar else tqdm(batch_iterator)
     for batch in iterator: 
         HVP_contribution = ComputeHVP(loss, par, batch, V, batch_parser=batch_parser) 
         HVP = accumulateHVP(HVP_contribution, HVP)
-        count+=batch_parser(batch)[0].shape[0]
+        count += 1
     HVP = jtu.tree_map(lambda x: x/count, HVP) 
     alpha = dot_product(HVP, HVP)/ dot_product(V, V)  
     V_new = normalize(HVP)
+    V_new = jtu.tree_map(lambda x, y : momentum*x + (1-momentum) * y, V, V_new)
     return V_new, alpha  
 
-def PowerMethodIterate(loss, par, batch_iterable, num_iterations, V0=None, batch_parser=standard_parser, show_pbar=False): 
+def PowerMethodIterate(loss, par, batch_iterable, num_iterations, V0=None, **kwargs): 
     
     if V0 == None:
         rng = jar.PRNGKey(0)
@@ -75,12 +81,16 @@ def PowerMethodIterate(loss, par, batch_iterable, num_iterations, V0=None, batch
     
     
     V = V0.copy() 
-    History = {'alpha' : [], 'iteration': []}
+    History = {'alpha' : [], 'iteration': [], 'q' : []}
     for iteration in range(num_iterations): 
        batch_iterator = iter(batch_iterable)  # Reset iterator for each iteration
-       V_new, alpha = PowerMethodStep(loss, par, batch_iterator, V, batch_parser=batch_parser, show_pbar=False) 
+       V_new, alpha = PowerMethodStep(loss, par, batch_iterator, V, **kwargs) 
+       q = dot_product(V, V_new)/( norm2(V)  * norm2(V_new))**0.5 
        V = V_new 
+       
        History['alpha'].append(alpha) 
        History['iteration'].append(iteration)
+       History['q'].append(q)
+
     return History
 
